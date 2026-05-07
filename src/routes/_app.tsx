0272cc875cab7dd7ad620f0dc8e81f2635633ce7
SidraFatima <sidrafatima9515@gmail.com>
2026-05-08 00:17:58 +0530

    Remove sidebar completely

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -20,23 +20,5 @@ function AppLayout() {
 
   if (loading || !user) return <FullSpinner />;
 
-  return (
-    <SidebarProvider>
-      <div className="min-h-screen flex w-full">
-        <AppSidebar />
-        <div className="flex-1 flex flex-col min-w-0">
-          <header className="h-14 flex items-center justify-between px-4 glass border-b border-sidebar-border sticky top-0 z-10">
-            <SidebarTrigger />
-            <div className="flex items-center gap-2">
-              <span className="hidden sm:inline text-sm text-muted-foreground">Hi, {user.name.split(" ")[0]}</span>
-              <ThemeToggle />
-            </div>
-          </header>
-          <main className="flex-1 p-4 sm:p-6 lg:p-8">
-            <Outlet />
-          </main>
-        </div>
-      </div>
-    </SidebarProvider>
-  );
+
 }
