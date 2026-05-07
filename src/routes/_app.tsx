4de9475a719542f58a740f2fbe97dbf47fded32d
SidraFatima <sidrafatima9515@gmail.com>
2026-05-08 00:33:12 +0530

    Fix app route export

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -1,7 +1,14 @@
-export default function AppLayout() {
+import { Outlet, createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_app")({
+  component: AppLayout,
+});
+
+function AppLayout() {
   return (
     <div>
       <h1>TaskFlow App</h1>
+      <Outlet />
     </div>
   );
 }
