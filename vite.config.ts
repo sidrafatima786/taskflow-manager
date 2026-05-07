8b2ee37e7c104366559e69d8aebb855522661a3d
SidraFatima <sidrafatima9515@gmail.com>
2026-05-08 00:42:26 +0530

    Fix Vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-
-  server: {
-    host: "0.0.0.0",
-    allowedHosts: true,
   },
 });
